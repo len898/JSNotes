c783e23f1efc8ea100d0699a436ed61c1c03fb19
lennart@familie-richter.de <lennart@familie-richter.de>
2023-03-03 22:29:57 -0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//Basic Function
+function sum(x,y){
+    return x+y;
+}
+
+function multiple(x,y){
+    return x*y;
+}
+
+console.log(sum(10,20));
+console.log(multiple(10,20));
+
+//Strings can be double or single quote
+"String 1";
+'string 2';
+
+console.log("String 1".length);
+
+let userName = "Lennart";
+console.log(userName.length);
+console.log(userName.toUpperCase());
+console.log(userName.toLowerCase());
+
