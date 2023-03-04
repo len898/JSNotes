b264beac73e8fea640901f62ac53f666021f27a3
lennart@familie-richter.de <lennart@familie-richter.de>
2023-03-03 22:53:35 -0800

    Second commit, string work mostly

--- script.js
+++ script.js
@@ -21,3 +21,28 @@ console.log(userName.length);
 console.log(userName.toUpperCase());
 console.log(userName.toLowerCase());
 
+console.log(userName[1]);
+console.log(userName[userName.length-2]);
+
+//Can use .at() to use negative indexing like python
+
+console.log(userName.charAt(-1));
+
+//Substring 2 paramters star and end, end being exclusive
+console.log(userName.substring(0, userName.length/2));
+console.log(userName.substring(userName.length/2));
+
+//Can use + for string concatenation, can also use += in the traditional way
+let lastName = "Richter";
+let fullName = userName + " " + lastName;
+console.log(fullName);
+
+//Template strings use backsterik character `
+`This is a template string`;
+let text = `Simple 
+multiline
+string`;
+
+//Template strings support interpolation
+let newName = "Marielle";
+console.log(`I am friends with ${newName}`);
